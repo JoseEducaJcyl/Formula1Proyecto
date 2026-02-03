6ab5d201f7632f847911dd1fe2c323f8ff898c4c
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-02-03 12:28:16 +0100

    Ejercicio sobre un proyecto de formula 1 leyendo y haciendo ficheros de texto

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,86 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Piloto> pilotos = new ArrayList<>();
+        cargarDatos(pilotos);
+        aniadirPiloto(pilotos);
+        mostrarDatos(pilotos);
+        buscarPiloto(pilotos);
+        guardarDatos(pilotos);
+    }
+    public static void cargarDatos(ArrayList<Piloto> pilotos) {
+        try (BufferedReader reader = new BufferedReader(new
+                FileReader("pilotos_f1.txt"))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                String[] campos = linea.split(";");
+                Piloto piloto = new Piloto(campos[0],campos[1], Integer.parseInt(campos[2]));
+                pilotos.add(piloto);
+            }
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al leer el archivo: " +
+                    e.getMessage());
+        }
+    }
+
+    public static void mostrarDatos(ArrayList<Piloto> pilotos) {
+        for (Piloto piloto : pilotos) {
+            System.out.println(piloto.getNombre());
+            System.out.println(piloto.getEscuderia());
+            System.out.println(piloto.getPuntos());
+            System.out.println(" ");
+        }
+    }
+
+    public static boolean comprobarPiloto(ArrayList<Piloto> pilotos, String piloto) {
+        boolean encontrado = false;
+        for (Piloto p : pilotos) {
+            if (p.getNombre().equals(piloto)) {
+                encontrado = true;
+            }
+        }
+        return encontrado;
+    }
+
+    public static void aniadirPiloto(ArrayList<Piloto> pilotos) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe el nombre del piloto");
+        String nombreP = sc.nextLine();
+        if (comprobarPiloto(pilotos, nombreP)) {
+            System.out.println("El piloto ya existe");
+        }else{
+            System.out.println("Escribe la escuderia del piloto");
+            String escuderiaP = sc.nextLine();
+            System.out.println("Escribe los puntos del piloto");
+            int puntosP = sc.nextInt();
+            pilotos.add(new Piloto(nombreP, escuderiaP, puntosP));
+        }
+    }
+
+    public static void buscarPiloto(ArrayList<Piloto> pilotos) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe el nombre del piloto");
+        String nombreP = sc.nextLine();
+        if (comprobarPiloto(pilotos, nombreP)) {
+            System.out.println("El piloto " +  nombreP + " existe");
+        }else {
+            System.out.println("No existe el piloto");
+        }
+    }
+
+    public static void guardarDatos(ArrayList<Piloto> pilotos) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("pilotos_f1_nuevo.txt"))) {
+            for (Piloto piloto : pilotos) {
+                writer.write(piloto.getNombre()+";"+piloto.getEscuderia()+";"+piloto.getPuntos());
+                writer.newLine();
+            }
+        }catch (IOException e) {
+            System.out.println("Ocurrió un error al escribir el archivo: " +
+                    e.getMessage());
+        }
+    }
+
 }
--- src/Piloto.java
+++ src/Piloto.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Piloto {
+    private String nombre;
+    private String escuderia;
+    private int puntos;
+
+    public Piloto(String nombre, String escuderia, int puntos) {
+        this.nombre = nombre;
+        this.escuderia = escuderia;
+        this.puntos = puntos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEscuderia() {
+        return escuderia;
+    }
+
+    public void setEscuderia(String escuderia) {
+        this.escuderia = escuderia;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
 }
